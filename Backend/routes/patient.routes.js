ace27f389006b7f456b1d7bf0cdba76d31aa1ba5
Sisco Falcó <siscofalcop@gmail.com>
2021-05-13 22:58:59 +0200

    delete patients on postman

--- Backend/routes/patient.routes.js
+++ Backend/routes/patient.routes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Patient = require('../models/Patient.model');
+const Doctor = require('../models/Doctor.model');
+const Activity = require('../models/Activity.model');
+const Session = require('../models/Session.model');
+const Diary = require('../models/Diary.model');
 
 // edit
 
@@ -33,8 +37,26 @@ router.get('/:id', (req, res, next) => {
 
 router.delete('/:id', (req, res, next) => {
     const { id } = req.params;
-    Patient.findOneAndRemove({ _id: id })
-    .then(() => res.status(200).json({message: "Patient deleted"}))
+    Patient.deleteOne({ _id: id })
+    .then((patient) => {
+        Doctor.updateOne({_id: patient.doctor}, { $pull: { patients: id } })
+        .then(() => {
+            Activity.deleteMany({patient: patient._id})
+            .then(() => {
+                Session.deleteMany({patient: patient._id})
+                .then(() => {
+                    Diary.deleteMany({patient: patient._id})
+                    .then(() => {
+                        return res.status(200).json({message: "Patient deleted"});
+                    })
+                    .catch(error => res.status(500).json(error))
+                })
+                .catch(error => res.status(500).json(error))
+            })
+            .catch(error => res.status(500).json(error))
+        })
+        .catch(error => res.status(500).json(error))
+    })
     .catch(error => res.status(500).json(error))
 })
 
