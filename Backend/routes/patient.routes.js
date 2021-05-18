e8ccd88cbd6b10f19c5729fbfac6a87bc331372f
Sisco Falcó <siscofalcop@gmail.com>
2021-05-18 21:47:26 +0200

    backend fixed

--- Backend/routes/patient.routes.js
+++ Backend/routes/patient.routes.js
@@ -28,7 +28,7 @@ module.exports = router;
 
 router.get('/:id', (req, res, next) => {
     const { id } = req.params;
-    Patient.findOne({ _id: id, doctor: req.session.currentUser._id }).populate(['activities', 'diary'])
+    Patient.findOne({ _id: id, doctor: req.session.currentUser._id }).populate(['activities', 'diary', 'sessions'])
     .then(data => res.status(200).json(data))
     .catch(error => res.status(500).json(error))
 })
