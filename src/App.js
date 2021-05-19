5346622542028098228c3a38b35d1d8098482540
Sisco Falcó <siscofalcop@gmail.com>
2021-05-19 18:17:55 +0200

    frontend fixes

--- src/App.js
+++ src/App.js
@@ -10,20 +10,21 @@ import './App.css';
 import DoctorProfile from './pages/profile/DoctorProfile';
 import PatientProfile from './pages/profile/PatientProfile';
 import PatientDetails from './pages/profile/PatientDetails';
+import NavbarComponent from './components/NavbarComponent';
 
 function App() {
   return (
     <div className="App">
-
-      <Route exact path="/" component={() => (<Redirect to="/login" />)} />
-      <PrivateRoute exact path="/user-selector" component={UserSelector} />
-      <PrivateRoute path="/doctor-profile/:id" component={DoctorProfile} />
-      <PrivateRoute path="/patient-profile/:id" component={PatientProfile} />
-      <PrivateRoute path="/patient-details/:id" component={PatientDetails} />
-      <PrivateRoute path="/new-patient" component={SignupPatient} />
-      <AnonRoute exact path="/signup" component={Signup} />
-      <AnonRoute exact path="/login" component={Login} />
-      <Route exact path="/logout" component={Logout} />
+      <NavbarComponent />
+        <Route exact path="/" component={() => (<Redirect to="/login" />)} />
+        <PrivateRoute exact path="/user-selector" component={UserSelector} />
+        <PrivateRoute path="/doctor-profile/:id" component={DoctorProfile} />
+        <PrivateRoute path="/patient-profile/:id" component={PatientProfile} />
+        <PrivateRoute path="/patient-details/:id" component={PatientDetails} />
+        <PrivateRoute path="/new-patient" component={SignupPatient} />
+        <AnonRoute exact path="/signup" component={Signup} />
+        <AnonRoute exact path="/login" component={Login} />
+        <Route exact path="/logout" component={Logout} />
     </div>
   );
 }
--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withAuth } from '../context/auth.context';
 
-class Navbar extends Component {
-    constructor(){
-        super()
+const Navbar = (props) => {
+
+    if (!props.user || !props.user.data || !props.user.data.username) {
+        return null;
     }
 
-    render(){
-        
+    const redirectTo = (url) => {
+        window.location.href = url;
     }
+
+    return (
+        <div className="navbar-container">
+            <div className="navbar-item" onClick={() => {redirectTo('/user-selector');}}>Profile</div>
+            {(props.user.data.patients && Array.isArray(props.user.data.patients) ? (<div className="navbar-item" onClick={() => {redirectTo('/new-patient');}}>New Patient</div>) : null)}
+            <div className="navbar-item" onClick={() => {redirectTo('/logout');}}>Logout</div>
+        </div>
+    )
 }
 
-export default Navbar;
+export default withAuth(Navbar);
